04fe7b03f5f152c1ef53d6925a9db3920b3fe7d9
Rajaram silwal <rajaramsilwal@Rajarams-MacBook-Air.local>
2026-02-19 10:30:17 +0545

    remaining

--- src/main/java/com/mycompany/networkprogrammingbyrajaram/question7.java
+++ src/main/java/com/mycompany/networkprogrammingbyrajaram/question7.java
@@ -3,5 +3,20 @@ package com.mycompany.networkprogrammingbyrajaram;
 import java.net.*;
 import java.util.*;
 public class question7 {
+    public static void main(String[] args) throws Exception{
+        String interfaceName= "en0";
+         NetworkInterface ni = NetworkInterface.getByName(interfaceName);
+         if (ni != null) {
+            Enumeration<InetAddress> addresses = ni.getInetAddresses();
+
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+                System.out.println("IP Address: " + addr.getHostAddress());
+            }
+        } else {
+            System.out.println("Interface not found.");
+        }
+
+    }
     
 }
